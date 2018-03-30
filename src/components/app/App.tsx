cbfaa8eb30094a823e19d04159b0589ad45ac8c3
Ismael Navarro <ismanapa@gmail.com>
2018-03-30 13:15:32 +0200

    Added react router

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TopBar from '../topBar';
 import RecipeList from '../recipeList';
+import RecipeDetail from '../recipeDetail';
 
 const style = require('./App.scss');
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <TopBar title={process.env.REACT_APP_NAME as string} />
-
-        <div className={style.container}>
-          <RecipeList/>
+      <Router>
+        <div>
+          <TopBar title={process.env.REACT_APP_NAME as string} />
+          <div className={style.container}>
+            <Route exact={true} path="/" component={RecipeList}/>
+            <Route path="/recipe" component={RecipeDetail}/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/recipeDetail/RecipeDetail.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const RecipeDetail = () => {
+    return <span>Recipe Detail</span>;
+};
+
+export default RecipeDetail;
