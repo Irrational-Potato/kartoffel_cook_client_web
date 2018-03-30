480ff70e9e0fac6a7a74bb77482884f2b7fcb050
Ismael Navarro <ismanapa@gmail.com>
2018-03-30 13:19:15 +0200

    Chaged App to function component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,20 +6,16 @@ import RecipeDetail from '../recipeDetail';
 
 const style = require('./App.scss');
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <TopBar title={process.env.REACT_APP_NAME as string} />
-          <div className={style.container}>
-            <Route exact={true} path="/" component={RecipeList}/>
-            <Route path="/recipe" component={RecipeDetail}/>
-          </div>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <TopBar title={process.env.REACT_APP_NAME as string} />
+      <div className={style.container}>
+        <Route exact={true} path="/" component={RecipeList} />
+        <Route path="/recipe" component={RecipeDetail} />
+      </div>
+    </div>
+  </Router>
+);
 
 export default App;
